3b8b5086a24854893698c19b19948a342d9e29f6
BlackChao-Script <83378274+BlackChao-Script@users.noreply.github.com>
2022-03-26 18:31:46 +0800

    修改sort接口返回数据

--- src/service/sort.service.js
+++ src/service/sort.service.js
@@ -1,5 +1,6 @@
 const Sort = require('../model/sort.model')
 const Goods = require('../model/goods.model')
+const { BASE_PATH } = require('../constant/data')
 
 class SortService {
   // 添加分类
@@ -23,7 +24,13 @@ class SortService {
   }
   // 获取分类详细数据
   async getSortServiceData(sort_id) {
-    return await Goods.findAll({ where: { sort_id } })
+    const res = await Goods.findAll({ where: { sort_id } })
+    res.forEach((element) => {
+      element.dataValues.goods_img = BASE_PATH + element.dataValues.goods_img
+    })
+    return {
+      res,
+    }
   }
 }
 
